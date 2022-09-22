4ea1c40037de4396ac203794197da95e44a5a9a0
hdc0854 <114062572+hdc0854@users.noreply.github.com>
2022-09-22 11:33:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,13 +62,14 @@ export const config = {
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "
+wx5f939d407f88668a",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "",
+      useTemplateId: "LT1qtaS16tbY5VflWL34kF3YVwdQhLt1BLzzvuyqIww",
       // 所在省份
       province: "贵州",
       // 所在城市
-      city: "龙里",
+      city: "贵阳",
       // 新历生日, 仅用作获取星座运势, 格式必须
       horoscopeDate: '12-27',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
@@ -87,9 +88,9 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: "2022-01-17"},
+        {"keyword": "love_day", date: "2022-05-12"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-01-17"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
